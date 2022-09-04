906a505074b6393906d2d492b42dfef259170953
daffasayekti <daffasayekti0206@gmail.com>
2022-09-04 21:29:16 +0700

    Gak mau comment

---
+++ public/assets/js/custom.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * You can write your JS code here, DO NOT touch the default style file
+ * because it will make it harder for you to update.
+ *
+ */
+
+"use strict";
+function submitDel(id){
+    $('#del-'+id);
+}
