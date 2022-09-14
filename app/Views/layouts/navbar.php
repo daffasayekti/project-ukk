b512e0d108765cf41fd3abb4bb20ab20a30b7029
daffasayekti <daffasayekti0206@gmail.com>
2022-09-14 14:08:27 +0700

    Ubah Navbar

--- app/Views/layouts/navbar.php
+++ app/Views/layouts/navbar.php
@@ -19,27 +19,81 @@
                             <a href="#" data-toggle="dropdown" class="nav-link nav-link-lg nav-link-user">
                                 <?php if (in_groups('User')) : ?>
                                     <img alt="image" src="/assets/images/profile_users/<?= user()->profile_img; ?>" class="rounded-circle mr-1" width="40" height="40">
+                                <?php elseif (in_groups('Admin')) :  ?>
+                                    <img alt="image" src="/assets/images/profile_users/<?= user()->profile_img; ?>" class="rounded-circle mr-1" width="40" height="40">
                                 <?php else : ?>
                                     <img alt="image" src="/assets/images/profile_users/default.png" class="rounded-circle mr-1" width="40" height="40">
                                 <?php endif; ?>
                             </a>
-                            <?php if (in_groups('User')) : ?>
-                                <div class="dropdown-menu dropdown-menu-right">
-                                    <a href="#" class="dropdown-item has-icon">
-                                        <i class="fas fa-user-cog"></i> Edit Profile
+                            <?php if (in_groups('User') && user()->jenis_akun_id == 1) : ?>
+                                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown" style="margin-top: 7px;">
+                                    <div class="text-center mb-2">
+                                        <img class="img-profile rounded-circle mb-2" src="/assets/images/profile_users/<?= user()->profile_img; ?>" width="40"><br>
+                                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><b><?= user()->username; ?></b></span>
+                                    </div>
+                                    <a href="#" data-toggle="modal" data-target="#editprofile" class="dropdown-item">
+                                        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Edit Profile
                                     </a>
-                                    <a href="<?= base_url('/home/berlangganan'); ?>" class="dropdown-item has-icon">
-                                        <i class="fas fa-money-bill"></i> Berlangganan
+                                    <a class="dropdown-item" href="<?= base_url('/admin/dashboard'); ?>">
+                                        <i class="fas fa-money-bill fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Langganan
                                     </a>
                                     <div class="dropdown-divider"></div>
-                                    <a href="<?= base_url('/logout'); ?>" class="dropdown-item has-icon text-danger">
-                                        <i class="fas fa-sign-out-alt"></i> Logout
+                                    <a class="dropdown-item" href="<?= base_url('/logout'); ?>">
+                                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-danger"></i>
+                                        Logout
+                                    </a>
+                                </div>
+                            <?php elseif (in_groups('User') && user()->jenis_akun_id == 2) : ?>
+                                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown" style="margin-top: 7px;">
+                                    <div class="text-center mb-2">
+                                        <img class="img-profile rounded-circle mb-2" src="/assets/images/profile_users/<?= user()->profile_img; ?>" width="40"><br>
+                                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><b><?= user()->username; ?></b></span>
+                                    </div>
+                                    <a href="#" data-toggle="modal" data-target="#editprofile" class="dropdown-item">
+                                        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Edit Profile
+                                    </a>
+                                    <a href="#" data-toggle="modal" data-target="#editprofile" class="dropdown-item">
+                                        <i class="fa-solid fa-gear fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Edit Post
+                                    </a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="<?= base_url('/logout'); ?>">
+                                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-danger"></i>
+                                        Logout
+                                    </a>
+                                </div>
+                            <?php elseif (in_groups('Admin')) : ?>
+                                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown" style="margin-top: 7px;">
+                                    <div class="text-center mb-2">
+                                        <img class="img-profile rounded-circle mb-2" src="/assets/images/profile_users/<?= user()->profile_img; ?>" width="40"><br>
+                                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><b><?= user()->username; ?></b></span>
+                                    </div>
+                                    <a href="#" data-toggle="modal" data-target="#editprofile" class="dropdown-item">
+                                        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Edit Profile
+                                    </a>
+                                    <a href="#" data-toggle="modal" data-target="#editprofile" class="dropdown-item">
+                                        <i class="fa-solid fa-user-tie fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        My Admin
+                                    </a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="<?= base_url('/logout'); ?>">
+                                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-danger"></i>
+                                        Logout
                                     </a>
                                 </div>
                             <?php else : ?>
-                                <div class="dropdown-menu dropdown-menu-right">
-                                    <a href="<?= base_url('/login'); ?>" class="dropdown-item has-icon">
-                                        <i class="fas fa-sign-in-alt"></i> Login
+                                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown" style="margin-top: 7px;">
+                                    <div class="text-center mb-2">
+                                        <img class="img-profile rounded-circle mb-2" src="/assets/images/profile_users/default.png" width="40"><br>
+                                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><b>Guest</b></span>
+                                    </div>
+                                    <a class="dropdown-item" href="<?= base_url('/login'); ?>">
+                                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-success"></i>
+                                        Login
                                     </a>
                                 </div>
                             <?php endif; ?>
